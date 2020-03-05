e305f2cd2c8386ea702319420e0ecb42da0ee98a
Larry Liang <lian999111@gmail.com>
2020-03-05 18:37:21 +0100

    Take deliver time into account during optimization

--- nested_jobshop.py
+++ nested_jobshop.py
@@ -16,9 +16,9 @@ def MinimalJobshopSat():
          ((2,), 1, 1, 1)],  # Job0
 
         [((0, 2), 1, 0, 0), 
-         ((1,), 4, 1, 1)],  # Job1
+         ((1,), 4, 1, 0)],  # Job1
 
-        [((1,), 4, 0, 0), 
+        [((1,), 4, 0, 26), 
          ((2,), 3, 1, 0)]  # Job2
     ]
 
@@ -29,10 +29,10 @@ def MinimalJobshopSat():
     horizon = sum(sum(task[1:]) for job in jobs_data for task in job)
 
     # Named tuple to store information about created variables.
-    task_type = collections.namedtuple('task_type', 'start delay_end interval')
+    task_type = collections.namedtuple('task_type', 'start process_end delay_end deliver_end interval')
     # Named tuple to manipulate solution information.
     assigned_task_type = collections.namedtuple('assigned_task_type',
-                                                'start job index processing delay')
+                                                'start job index process_end delay_end deliver_end')
 
     # Creates job intervals and add to the corresponding machine lists.
     all_tasks = {}
@@ -44,13 +44,15 @@ def MinimalJobshopSat():
             duration = task[1] + task[2]    # processing + delay
             suffix = '_%i_%i' % (job_id, task_id)
             start_var = model.NewIntVar(0, horizon, 'start' + suffix)
+            process_var = model.NewIntVar(0, horizon, 'process_end' + suffix)
             delay_var = model.NewIntVar(0, horizon, 'delay_end' + suffix)
+            deliver_var = model.NewIntVar(0, horizon, 'deliver_end' + suffix)
 
             # Each interval includes its processing + delay
             interval_var = model.NewIntervalVar(start_var, duration, delay_var,
                                                 'interval' + suffix)
             all_tasks[job_id, task_id] = task_type(
-                start=start_var, delay_end=delay_var, interval=interval_var)
+                start=start_var, process_end=process_var, delay_end=delay_var, deliver_end=deliver_var, interval=interval_var)
             
             for machine in occup_machines:
                 machine_to_intervals[machine].append(interval_var)
@@ -59,17 +61,32 @@ def MinimalJobshopSat():
     for machine in all_machines:
         model.AddNoOverlap(machine_to_intervals[machine])
 
-    # Precedences inside a job, including delivery constraints
+    # Set delay constraints
+    for job_id, job in enumerate(jobs_data):
+        for task_id in range(len(job)):
+            # A task must wait for its preceeding task to finish and deliver
+            delay_time = job[task_id][2]
+            model.Add(all_tasks[job_id, task_id].delay_end == all_tasks[job_id, task_id].process_end + delay_time)
+
+    # Set deliver constraints
+    for job_id, job in enumerate(jobs_data):
+        for task_id in range(len(job)):
+            # A task must wait for its preceeding task to finish and deliver
+            deliver_time = job[task_id][3]
+            model.Add(all_tasks[job_id, task_id].deliver_end == all_tasks[job_id, task_id].delay_end + deliver_time)
+
+    # Precedences inside a job
     for job_id, job in enumerate(jobs_data):
         for task_id in range(len(job) - 1):
             # A task must wait for its preceeding task to finish and deliver
             deliver_time = job[task_id][3]
-            model.Add(all_tasks[job_id, task_id + 1].start >= all_tasks[job_id, task_id].delay_end + deliver_time)
+            model.Add(all_tasks[job_id, task_id + 1].start >= all_tasks[job_id, task_id].deliver_end)
 
     # Makespan objective.
+    # The Makespan includes deliver time
     obj_var = model.NewIntVar(0, horizon, 'makespan')
     model.AddMaxEquality(obj_var, [
-        all_tasks[job_id, len(job) - 1].delay_end
+        all_tasks[job_id, len(job) - 1].deliver_end
         for job_id, job in enumerate(jobs_data)
     ])
     model.Minimize(obj_var)
@@ -90,8 +107,9 @@ def MinimalJobshopSat():
                             start=solver.Value(all_tasks[job_id, task_id].start),
                             job=job_id,
                             index=task_id,
-                            processing=task[1],
-                            delay=task[2]))
+                            process_end=solver.Value(all_tasks[job_id, task_id].process_end),
+                            delay_end=solver.Value(all_tasks[job_id, task_id].delay_end),
+                            deliver_end=solver.Value(all_tasks[job_id, task_id].deliver_end))                            )
 
         # Create per machine output lines.
         output = ''
@@ -107,14 +125,12 @@ def MinimalJobshopSat():
                 sol_line_tasks += '%-10s' % name
 
                 start = assigned_task.start
-                processing = assigned_task.processing
-                delay = assigned_task.delay
+                process_end = assigned_task.process_end
+                delay_end = assigned_task.delay_end
+                deliver_end = assigned_task.deliver_end
 
-                process_end = start + processing
-                delay_end = process_end + delay
-
-                # Format for each task: [start, process_end, delay_end]
-                sol_tmp = '[%i,%i,%i]' % (start, process_end, delay_end)
+                # Format for each task: [start, process_end, delay_end, deliver_end]
+                sol_tmp = '[%i,%i,%i,%i]' % (start, process_end, delay_end, deliver_end)
                 # Add spaces to output to align columns.
                 sol_line += '%-10s' % sol_tmp
 
